dd21f6f98f27be3210afa397a5d3eb2b4ce04c37
Laxerk <luismarcelomoreno7@gmail.com>
2026-01-28 08:34:04 -0600

    archivos js
    cree archivos js para tener los datos de los carros por ahora, hice el index tambien

---
+++ autos.js
@@ -0,0 +1,47 @@
+const autos = [
+  {
+    id: 1,
+    marca: "Toyota",
+    modelo: "Corolla",
+    año: 2024,
+    tipo: "Sedán",
+    precio: 350000,
+    imagen: ""
+  },
+  {
+    id: 2,
+    marca: "Honda",
+    modelo: "Civic",
+    año: 2024,
+    tipo: "Sedán",
+    precio: 370000,
+    imagen: ""
+  },
+  {
+    id: 3,
+    marca: "Mazda",
+    modelo: "CX-5",
+    año: 2024,
+    tipo: "SUV",
+    precio: 480000,
+    imagen: ""
+  },
+  {
+    id: 4,
+    marca: "Ford",
+    modelo: "Mustang",
+    año: 2024,
+    tipo: "Deportivo",
+    precio: 850000,
+    imagen: ""
+  },
+  {
+    id: 5,
+    marca: "Chevrolet",
+    modelo: "Onix",
+    año: 2024,
+    tipo: "Sedán",
+    precio: 290000,
+    imagen: ""
+  }
+];
